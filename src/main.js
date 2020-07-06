dd356fffaf6c15e2266a5672d277efe7f9fdcd89
George Tsilikas <tsili.g@gmail.com>
2020-07-06 04:25:26 +0300

    first version

---
+++ src/main.js
@@ -0,0 +1,38 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+// Import global styles
+require('~/main.css')
+
+import DefaultLayout from '~/layouts/Default.vue'
+import VueScrollTo from 'vue-scrollto'
+
+export default function (Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+
+  Vue.use(VueScrollTo, {
+    duration: 500,
+    easing: "ease",
+  })
+
+  head.meta.push({
+    name: 'keywords',
+    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+  })
+
+  head.meta.push({
+    name: 'description',
+    content: 'Personal website created with Gridsome and Tailwind'
+  })
+
+  head.meta.push({
+    name: 'author',
+    content: 'George Tsilikas'
+  })
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
+  })
+}
---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  purge: {
+    content: [
+        './src/**/*.vue',
+        './src/**/*.js',
+        './src/**/*.jsx',
+        './src/**/*.html',
+        './src/**/*.pug',
+        './src/**/*.md',
+    ],
+    whitelist: [
+        'body',
+        'html',
+        'img',
+        'a',
+        'g-image',
+        'g-image--lazy',
+        'g-image--loaded',
+        'active'
+    ],
+    extractors: [
+        {
+            extractor: content => content.match(/[A-z0-9-:\\/]+/g),
+            extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
+        },
+    ],
+},
+  theme: {
+    container: {
+      padding:'1rem'
+    },
+    fontFamily: {
+      sans: [
+        'Roboto',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+    },
+    extend: {
+      borderWidth: {
+        '14': '14px'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
