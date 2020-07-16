684458a5abb534eb8f48549a0231b24813fbebbb
George Tsilikas <tsili.g@gmail.com>
2020-07-16 23:35:14 +0300

    more testing

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,7 +3,7 @@
     <header class="border-t-14 border-green-700">
       <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
         <div>
-          <g-link to="/"><g-image src="@/assets/images/logo.png" class="w-40" alt="logo" /></g-link>
+          <g-link to="/"><g-image src="~/logo.png" class="w-40" alt="logo" /></g-link>
         </div>
         <div class="block lg:hidden">
           <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
